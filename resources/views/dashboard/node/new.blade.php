e654bf93df7578294877561c1d60539208809444
cravindr <cravindr@gmail.com>
2020-03-02 15:52:29 +0530

    By Ravi

--- resources/views/dashboard/node/new.blade.php
+++ resources/views/dashboard/node/new.blade.php
@@ -4,7 +4,7 @@
 
 @section('pagecontent')
     <div class="row">
-        <div class="col-lg-4 offset-lg-4 col-md-4 offset-md-4">
+        <div class="col-lg-6 offset-lg-3 col-md-6 offset-md-3">
             <div class="card card-primary">
                 <div class="card-header">
                     Coupon Generation
@@ -36,7 +36,7 @@
                             <label for="u_name">D.O.B</label>
 
                             <input type="date" name="dob" id="dob" class="form-control"
-                                   value="" placeholder="Enter Father/Spouse Name">
+                                   value="" placeholder="Select D.O.B">
                         </div>
                         <div class="form-group">
                             <label for="u_name">Mobile Number</label>
@@ -44,10 +44,73 @@
                             <input type="text" name="mobile" id="mobile" class="form-control"
                                    value="" placeholder="Enter Mobile Number">
                         </div>
+
+                        <div class="form-group">
+                            <label for="u_name">Aadhar Number</label>
+
+                            <input type="text" name="aadhar" id="aadhar" class="form-control"
+                                   value="" placeholder="Enter Aadhar Number">
+                        </div>
+                        <div class="form-group">
+                            <label for="lpan">Pan Number</label>
+
+                            <input type="text" name="pan" id="pan" class="form-control"
+                                   value="" placeholder="Enter PAN Number">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="u_email">Address</label>
+                            <textarea name="address" id="address" class="form-control"
+                                      value="" placeholder="Address"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="u_email">Account Number</label>
+                            <input type="text" name="accno" id="accno" class="form-control"
+                                   value="" placeholder="Enter Account Number">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="u_email">IFSC Code</label>
+                            <input type="text" name="ifsccode" id="ifsccode" class="form-control"
+                                   value="" placeholder="Enter IFSC Code">
+                        </div>
+                        <div class="form-group">
+                            <label for="u_email">Bank Name</label>
+                            <input type="text" name="bankname" id="bankname" class="form-control"
+                                   value="" placeholder="Enter Bank Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="u_email">Branch Name</label>
+                            <input type="text" name="branchname" id="branchname" class="form-control"
+                                   value="" placeholder="Enter Branch Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="u_email">Nominee Name</label>
+                            <input type="text" name="nominee" id="nominee" class="form-control"
+                                   value="" placeholder="Enter Nominee Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="u_email">Relationship with Nominee Name</label>
+                            <input type="text" name="relationship" id="relationship" class="form-control"
+                                   value="" placeholder="Enter Nominee Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="u_email">Sponser Name</label>
+                            <input type="text" name="sponsername" id="sponsername" class="form-control"
+                                   value="" placeholder="Enter Sponser Name">
+                        </div>
+
                         <div class="form-group">
                             <label for="u_email">Email</label>
                             <input type="text" name="email" id="email" class="form-control"
                                    value="" placeholder="Enter Email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="u_email">Mobile</label>
+                            <input type="text" name="mobile" id="mobile" class="form-control"
+                                   value="" placeholder="Enter Mobile Number">
                         </div>
 
                         <div class="form-group">
